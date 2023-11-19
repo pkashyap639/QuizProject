4ad6d73784cb1c092ff16d53bee24f4af949db2e
pkashyap639 <138331205+pkashyap639@users.noreply.github.com>
2023-11-19 06:39:46 -0500

    djongo setup

--- quizproject/quizproject/settings.py
+++ quizproject/quizproject/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'quizproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        "CLIENT": {
+           "name": 'quizproject',
+           "host": 'mongodb+srv://pkashyap148:*Password123@quizcluster.dzltjq0.mongodb.net/?authMechanism=DEFAULT',
+           "username": 'pkashyap148',
+           "password": '*Password123',
+           "authMechanism": "SCRAM-SHA-1",
+        }, 
     }
 }
 
