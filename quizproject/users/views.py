a73e5da2bc666af4fd70e915a015261382259080
pkashyap639 <138331205+pkashyap639@users.noreply.github.com>
2023-11-19 09:57:43 -0500

    signup form

--- quizproject/users/views.py
+++ quizproject/users/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import UserRegistrationForm
+from django.contrib import messages
+from django.http import HttpResponse
+
 
 # Create your views here.
 def register(req):
-    return render(req,'signup.html')
+    if req.method == 'POST':
+        form = UserRegistrationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            return HttpResponse("Saved")
+    else:
+        form = UserRegistrationForm()
+    return render(req,'signup.html',{'form':form})
